b7abf200211d97f6bd16740fe0839827dfa272ba
Vex Lorrane <vexlorrane@gmail.com>
2023-08-17 19:49:27 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "v-space-42914",
   url: "https://v-space-42914.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,64 +1,202 @@
-import React from "react"
-import {
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet
-} from "react-native"
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView, StyleSheet, TouchableOpacity, Modal, Pressable, TextInput } from "react-native";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
+  const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [navigateToHomePage, setNavigateToHomePage] = useState(false);
+
+  const handleRegister = () => {
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
+  const handleCompleteRegistration = () => {
+    if (email && password) {
+      // Perform registration logic here
+      setModalVisible(false);
+      setNavigateToHomePage(true);
+    } else {// Display error message or validation
+    }
+  };
+
+  const handleOpenHomePage = () => {
+    navigation.navigate("activityFeed");
+  };
+
+  return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.group} />
         <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
+          <Text style={styles.title}>V Space</Text>
+          <Text style={styles.subtitle}>A place to make school great.</Text>
+          <TouchableOpacity style={styles.buttonBlue} onPress={handleRegister}>
+            <Text style={styles.buttonTextBlue}>Register</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonWhite} onPress={handleRegister}>
+            <Text style={styles.buttonTextWhite}>Log in</Text>
+          </TouchableOpacity>
         </View>
-        <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
       </ScrollView>
-    </SafeAreaView>
-  )
-}
+
+      <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
+
+      <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={handleCloseModal}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Registration</Text>
+            <Text style={styles.modalText}>Enter your school email here:</Text>
+            <TextInput style={styles.input} value={email} onChangeText={setEmail} placeholder="School Email" placeholderTextColor="#999" />
+            <Text style={styles.modalText}>Password:</Text>
+            <TextInput style={styles.input} value={password} onChangeText={setPassword} placeholder="Password" placeholderTextColor="#999" secureTextEntry />
+            <TouchableOpacity style={styles.completeButton} onPress={handleCompleteRegistration}>
+              <Text style={styles.completeButtonText}>
+                Complete Registration
+              </Text>
+            </TouchableOpacity>
+            <Pressable style={styles.modalButton} onPress={handleOpenHomePage}>
+              <Text style={styles.modalButtonText}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      {navigateToHomePage}
+    </SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#F8F8FC",
-    height: "100%"
+    height: "100%",
+    justifyContent: "space-between"
   },
   scrollView: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "center",
     padding: 20
   },
   group: {
     alignItems: "center"
   },
-  logo: {
-    height: 180,
-    width: 180,
-    padding: 40,
-    borderRadius: 30,
-    margin: 40
+  title: {
+    fontSize: 48,
+    fontWeight: "bold",
+    color: "#000",
+    marginBottom: 10,
+    fontFamily: "Arial",
+    borderRadius: 10
   },
-  text: {
+  subtitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000",
+    fontFamily: "Arial",
+    borderRadius: 10,
+    textAlign: "center"
+  },
+  buttonBlue: {
+    backgroundColor: "#fb95ca",
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 20
+  },
+  buttonTextBlue: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold",
     textAlign: "center",
-    fontSize: 28,
-    color: "#828AB0",
-    fontWeight: 700
+    fontFamily: "Arial"
+  },
+  buttonWhite: {
+    backgroundColor: "#FFF",
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 10
+  },
+  buttonTextWhite: {
+    color: "#333",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "Arial"
   },
   footer: {
     textAlign: "center",
-    fontSize: 18,
+    fontSize: 14,
     color: "#828AB0",
-    fontWeight: 700,
+    fontWeight: "bold",
     marginBottom: 20
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)"
+  },
+  modalContent: {
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    padding: 20
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000",
+    marginBottom: 10,
+    fontFamily: "Arial",
+    textAlign: "center"
+  },
+  modalText: {
+    fontSize: 16,
+    color: "#000",
+    fontFamily: "Arial",
+    marginBottom: 10,
+    textAlign: "center"
+  },
+  input: {
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 20
+  },
+  completeButton: {
+    backgroundColor: "#FF69B4",
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignSelf: "center",
+    marginBottom: 10
+  },
+  completeButtonText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "Arial"
+  },
+  modalButton: {
+    backgroundColor: "#007BFF",
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignSelf: "center"
+  },
+  modalButtonText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "Arial"
   }
-})
-
-export default WelcomeScreen
+});
+export default WelcomeScreen;
