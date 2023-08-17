66fd3cb911769bfa4202555887e1f14048ab2527
Vex Lorrane <vexlorrane@gmail.com>
2023-08-17 19:58:18 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "v-space-42914",
-  url: "https://v-space-42914.botics.co",
-  api: "https://v-space-42914.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "v-space-42914",
+  "url": "https://v-space-42914.botics.co",
+  "api": "https://v-space-42914.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "activityFeed"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
